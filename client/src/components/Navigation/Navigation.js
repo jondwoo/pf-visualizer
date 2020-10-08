5fc9d31ddb32e754dc650a7250e831e90392aef5
Jon <jondwoo@gmail.com>
2020-10-08 11:01:09 -0700

    fix navbar toggler

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -5,26 +5,49 @@ import './Navigation.css';
 const Navigation = () => {
   return (
     // fix drop down
+    // <nav className="navbar navbar-expand-lg navbar-light">
+    //   <div className="container">
+    //     <a className="navbar-brand" href="/">
+    //       PFV
+    //     </a>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-toggle="collapse"
+    //       data-target="#navbarSupportedContent"
+    //       aria-controls="navbarSupportedContent"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //       <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+    //         <li className="nav-item">
+    //           <a className="nav-link active" aria-current="page" href="/">
+    //             Home
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="/">
+        <a href="#" className="navbar-brand">
           PFV
         </a>
         <button
           className="navbar-toggler"
-          type="button"
           data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          data-target="#navbarMenu"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navbarMenu">
+          <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link" href="">
                 Home
               </a>
             </li>
