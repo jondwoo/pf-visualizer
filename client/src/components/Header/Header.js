5f2ae911fee7d09c906662e2562da9664fba4804
Jon Ham <jondwoo@gmail.com>
2020-10-30 18:05:29 -0700

    fix dropdown

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,130 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
+import axios from 'axios';
 
+import { sortDesc } from '../../util/index';
 import Selector from './Selector/Selector';
 import './Header.css';
 
 const Header = () => {
+  const [monthOptionTemplate, setMonthOptionTemplate] = useState();
+  const [yearOptionTemplate, setYearOptionTemplate] = useState();
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [monthDefault, setMonthDefault] = useState();
+  const [yearDefault, setYearDefault] = useState();
+
+  const [apiData, setApiData] = useState();
+
+  const monthName = useMemo(() => {
+    return [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+  }, []);
+
+  const getYearOptions = useCallback(() => {
+    if (apiData) {
+      const years = apiData.map((entry) => {
+        return entry._id.year;
+      });
+
+      const sortedYears = sortDesc(years);
+
+      const yearOptions = [];
+
+      for (const year of sortedYears) {
+        yearOptions.push({
+          value: year,
+          label: year,
+        });
+      }
+
+      setYearDefault(yearOptions[0]);
+      return yearOptions;
+    }
+    return null;
+  }, [apiData]);
+
+  const getMonthOptions = useCallback(
+    (year) => {
+      if (apiData) {
+        const monthObjList = [];
+        const monthList = [];
+        const monthByName = [];
+
+        for (const entry of apiData) {
+          if (entry._id.year === parseInt(year)) {
+            monthObjList.push(entry.months);
+          }
+        }
+
+        for (const monthObj of monthObjList[0]) {
+          monthList.push(monthObj.month);
+        }
+
+        const sortedMonthList = sortDesc(monthList);
+
+        for (const month of sortedMonthList) {
+          monthByName.push(monthName[month - 1]);
+        }
+
+        const monthOptions = [];
+
+        for (const month of monthByName) {
+          monthOptions.push({
+            value: month,
+            label: month,
+          });
+        }
+
+        return monthOptions;
+      }
+
+      return null;
+    },
+    [monthName, apiData],
+  );
+
+  const handleChange = ({ value }) => {
+    setSelectedYear(value);
+  };
+
+  // set api data
+  useEffect(() => {
+    const fetchApiData = async () => {
+      try {
+        const timeData = await axios.get('http://localhost:9000/list/timeData');
+        setApiData(timeData.data.timeframe);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchApiData();
+  }, []);
+
+  // extract available months based on selected year
+  useEffect(() => {
+    const yearOptions = getYearOptions();
+    setYearOptionTemplate(yearOptions);
+
+    if (selectedYear) {
+      const monthOptions = getMonthOptions(selectedYear);
+      if (monthOptions) {
+        setMonthOptionTemplate(monthOptions);
+        setMonthDefault(monthOptions[0]);
+      }
+    }
+  }, [apiData, getYearOptions, getMonthOptions, selectedYear]);
+
   return (
     <>
       {/* HEADER */}
@@ -17,10 +138,21 @@ const Header = () => {
                 {/* PRETITLE */}
                 <h6 className="header-pretitle text-uppercase">Overview</h6>
                 {/* SELECTORS */}
-                <Selector timeframe="month" />
+                {monthOptionTemplate && (
+                  <Selector
+                    monthOptions={monthOptionTemplate}
+                    defaultValue={monthDefault}
+                  />
+                )}
               </div>
               <div className="col col-12 col-xl-2 col-lg-3 mb-3 mb-lg-0">
-                <Selector timeframe="year" />
+                {yearOptionTemplate && (
+                  <Selector
+                    yearOptions={yearOptionTemplate}
+                    handleChange={handleChange}
+                    defaultValue={yearDefault}
+                  />
+                )}
               </div>
               {/* CREATE BUTTON */}
               <div className="col-12 ml-auto col-lg-auto">
--- client/src/components/Header/Selector/Selector.js
+++ client/src/components/Header/Selector/Selector.js
@@ -1,156 +1,30 @@
-import React, { useEffect, useState, useCallback, useMemo } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 
-import { sortDesc } from '../../../util/index';
-
-const Selector = ({ timeframe }) => {
-  const [monthOptionTemplate, setMonthOptionTemplate] = useState();
-  const [yearOptionTemplate, setYearOptionTemplate] = useState();
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-
-  const [apiData, setApiData] = useState();
-
-  const monthName = useMemo(() => {
-    return [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-  }, []);
-
-  const getYearOptions = useCallback(() => {
-    if (apiData) {
-      const years = apiData.map((entry) => {
-        return entry._id.year;
-      });
-
-      const sortedYears = sortDesc(years);
-
-      const yearOptions = [];
-
-      for (const year of sortedYears) {
-        yearOptions.push({
-          value: year,
-          label: year,
-        });
-      }
-
-      return yearOptions;
-    }
-    // return [{ value: 'year', label: 'Year' }];
-    return null;
-  }, [apiData]);
-
-  const getMonthOptions = useCallback(
-    (year) => {
-      if (apiData) {
-        const monthObjList = [];
-        const monthList = [];
-        const monthByName = [];
-
-        for (const entry of apiData) {
-          if (entry._id.year === parseInt(year)) {
-            monthObjList.push(entry.months);
-          }
-        }
-
-        for (const monthObj of monthObjList[0]) {
-          monthList.push(monthObj.month);
-        }
-
-        const sortedMonthList = sortDesc(monthList);
-
-        for (const month of sortedMonthList) {
-          monthByName.push(monthName[month - 1]);
-        }
-
-        const monthOptions = [];
-
-        for (const month of monthByName) {
-          monthOptions.push({
-            value: month,
-            label: month,
-          });
-        }
-
-        return monthOptions;
-      }
-
-      // return [{ value: 'month', label: 'Month' }];
-      return null;
-    },
-    [monthName, apiData],
-  );
-
-  const handleChange = ({ value }) => {
-    setSelectedYear(value);
-  };
-
-  // set api data
-  useEffect(() => {
-    const fetchApiData = async () => {
-      try {
-        const timeData = await axios.get('http://localhost:9000/list/timeData');
-        setApiData(timeData.data.timeframe);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchApiData();
-  }, []);
-
-  // extract available months based on selected year
-  useEffect(() => {
-    const yearOptions = getYearOptions();
-    setYearOptionTemplate(yearOptions);
-
-    const monthOptions = getMonthOptions(selectedYear);
-    setMonthOptionTemplate(monthOptions);
-  }, [apiData, getYearOptions, getMonthOptions, selectedYear]);
-
-  if (timeframe === 'year') {
-    if (yearOptionTemplate) {
-      return (
-        <Select
-          options={yearOptionTemplate}
-          onChange={handleChange}
-          defaultValue={yearOptionTemplate[0]}
-        />
-      );
-    }
-    return <Select />;
-  } else if (timeframe === 'month') {
-    if (monthOptionTemplate) {
-      return (
-        <Select
-          options={monthOptionTemplate}
-          // defaultValue={monthOptionTemplate[0]}
-        />
-      );
-    }
-    return <Select />;
-  } else {
-    return <p>test</p>;
+const Selector = ({
+  monthOptions,
+  yearOptions,
+  handleChange,
+  defaultValue,
+}) => {
+  if (monthOptions) {
+    return <Select options={monthOptions} defaultValue={defaultValue} />;
   }
+  return (
+    <Select
+      options={yearOptions}
+      onChange={handleChange}
+      defaultValue={defaultValue}
+    />
+  );
 };
 
 export default Selector;
 
 Selector.propTypes = {
-  timeframe: PropTypes.string,
+  monthOptions: PropTypes.array,
+  yearOptions: PropTypes.array,
+  handleChange: PropTypes.func,
+  defaultValue: PropTypes.object,
 };
-
-// TODO: default selected year not showing
-// months not changing
-// refactor to NOT get initial values. set values when user selects a year
